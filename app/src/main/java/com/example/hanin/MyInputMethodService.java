545a1b8f7fbbb8efecb3d1086e443eb4526d0839
jayroop <jayrmh@gmail.com>
2019-04-26 19:07:19 +0400

    1

---
+++ app/src/main/java/com/example/hanin/MyInputMethodService.java
@@ -0,0 +1,79 @@
+package com.example.hanin;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    @Override
+    public View onCreateInputView() {
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.number_pad);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+
+        if (inputConnection != null) {
+            switch(primaryCode) {
+                case Keyboard.KEYCODE_DELETE :
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0);
+                    } else {
+                        inputConnection.commitText("", 1);
+                    }
+
+                    break;
+                default :
+                    char code = (char) primaryCode;
+                    inputConnection.commitText(String.valueOf(code), 1);
+            }
+        }
+
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
